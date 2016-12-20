50466a8d0e21ddcc7174c7051d993f809f1193fc
Ryo Miyajima <sergeant.wizard@gmail.com>
2016-12-20 09:46:19 +0900

    move particular windows to predefined spaces

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,21 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Config.Gnome
+import qualified XMonad.StackSet as W   -- Managehook rules, for unfloat
+import XMonad.Util.EZConfig
 
+myManageHook = composeAll
+   [ className =? "Slack"                               --> doShift "9:slack"
+   , className =? "Google-chrome" <&&> title =? "LINE"  --> doShift "8:line"
+   , className =? "Google-chrome" --> doShift "2:web"
+   , manageDocks
+   ]
+
+-- main = xmonad $ gnomeConfig
 main = xmonad $ gnomeConfig
     { borderWidth = 3
+    , workspaces = ["1:dev","2:web","3","4","5","6","7","8:line","9:slack"]
     , modMask = mod4Mask
     , terminal = "urxvt"
+    , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
     }
