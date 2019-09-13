562f105b3ffd317e2478bf3041c3c92504bafd44
Ryo Miyajima <sergeant.wizard@gmail.com>
2019-09-13 10:23:14 -0700

    add ubuntu setup script

---
+++ ubuntu_setup.sh
@@ -0,0 +1,9 @@
+sudo apt install -y curl git tmux silversearcher-ag
+curl https://pyenv.run | bash
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pushd
+git clone git@github.com:sergeant-wizard/rc_files
+ln -s rc_files/.vimrc .
+ln -s rc_files/tmux.conf.linux .tmux.conf
+git clone git@github.com:vim/vim
+git clone git@github.com:powerline/powerline
